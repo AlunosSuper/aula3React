88077d04a9b8ccfc4c4b4241513944eb80f9466d
Aluno Super <alunossupergeeksjoinville@gmail.com>
2026-03-09 19:14:29 -0300

    Editando os componentes

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,6 +1,5 @@
 import { Titulo } from "./componentes/Titulo"
 import { Campo } from "./componentes/Campo"
-import { Conteudo } from "./componentes/Conteudo"
 
 export function Inicio() {
   return (
@@ -8,12 +7,9 @@ export function Inicio() {
       <Titulo nome="Velejando com o React" />
 
       <Campo id="1">
-        <Conteudo texto="Estudar React"/>
       </Campo>
 
       <Campo id="2">
-        <Conteudo texto="Tirar o lixo"/>
-        <Conteudo texto="Lavar a louça"/>
       </Campo>
     </>
   )
